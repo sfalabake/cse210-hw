a7ddd166166daafa6af8b5e1ce3ca07b2cc36245
sfalabake <sfalabake@byupathway.edu>
2026-01-10 11:59:45 +0100

    Task 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,11 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        // Create a random number generator
+        Random randomGenerator = new Random();
+
+        // Generate magic number between 1 and 100
+        int magicNumber = randomGenerator.Next(1, 101);
 
         int guess = -1;
 
+        // Loop until the correct number is guessed
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
