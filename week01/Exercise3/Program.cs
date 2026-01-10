3cbc84356c9dde20e7b132e8c3bfc352b71d94a2
sfalabake <sfalabake@byupathway.edu>
2026-01-10 11:56:27 +0100

    Task 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,20 +7,25 @@ class Program
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        int guess = -1;
 
-        if (guess < magicNumber)
+        while (guess != magicNumber)
         {
-            Console.WriteLine("Higher");
-        }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
