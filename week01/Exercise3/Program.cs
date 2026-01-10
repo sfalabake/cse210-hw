f65177a31af82f3b83b6b02c055a62e54e1b49e2
sfalabake <sfalabake@byupathway.edu>
2026-01-10 12:02:44 +0100

    Challenge 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,19 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a random number generator
         Random randomGenerator = new Random();
-
-        // Generate magic number between 1 and 100
         int magicNumber = randomGenerator.Next(1, 101);
 
         int guess = -1;
+        int numberOfGuesses = 0;
 
-        // Loop until the correct number is guessed
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
+            numberOfGuesses++;
 
             if (guess < magicNumber)
             {
@@ -31,5 +29,7 @@ class Program
                 Console.WriteLine("You guessed it!");
             }
         }
+
+        Console.WriteLine($"It took you {numberOfGuesses} guesses.");
     }
 }
